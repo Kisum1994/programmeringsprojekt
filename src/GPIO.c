912b04b5118f6bc04c43c455dded91dd37e85813
Kisum1994 <46593938+Kisum1994@users.noreply.github.com>
2019-01-11 10:51:12 +0100

    Add files via upload

---
+++ src/GPIO.c
@@ -0,0 +1,518 @@
+#include "30010_io.h"
+
+#include "GPIO.h"
+#include "ansi.h"
+#include "charset.h"
+#include "lut.h"
+
+#define ESC 0x1B
+    int time;
+    int s;
+    int s_;
+    int m;
+    int h;
+    int buffCount=0;
+
+void startUP() {
+    int time;
+    int s;
+    int s_;
+    int m;
+    int h;
+    int buffCount=0;
+}
+
+void initJoystick () {
+ RCC->AHBENR |= RCC_AHBPeriph_GPIOA;
+ RCC->AHBENR |= RCC_AHBPeriph_GPIOB;
+ RCC->AHBENR |= RCC_AHBPeriph_GPIOC;
+  // Enable clock for GPIO Port A
+                        // Set pin PA4 to input
+ GPIOA->MODER &= ~(0x00000003 << (4 * 2)); // Clear mode register
+ GPIOA->MODER |= (0x00000000 << (4 * 2));
+ /* Set mode register (0x00 – Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out) */
+ GPIOA->PUPDR &= ~(0x00000003 << (4 * 2)); // Clear push/pull register
+ GPIOA->PUPDR |= (0x00000002 << (4 * 2));
+ /* Set push/pull register (0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down) */
+
+                        // Set pin PB0 to input
+ GPIOB->MODER &= ~(0x00000003 << (0 * 2)); // Clear mode register
+ GPIOB->MODER |= (0x00000000 << (0 * 2));
+ /* Set mode register (0x00 – Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out) */
+ GPIOB->PUPDR &= ~(0x00000003 << (0 * 2)); // Clear push/pull register
+ GPIOB->PUPDR |= (0x00000002 << (0 * 2));
+ /* Set push/pull register (0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down) */
+
+                        // Set pin PB5 to input
+ GPIOB->MODER &= ~(0x00000003 << (5 * 2)); // Clear mode register
+ GPIOB->MODER |= (0x00000000 << (5 * 2));
+ /* Set mode register (0x00 – Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out) */
+ GPIOB->PUPDR &= ~(0x00000003 << (5 * 2)); // Clear push/pull register
+ GPIOB->PUPDR |= (0x00000002 << (5 * 2));
+ /* Set push/pull register (0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down) */
+
+                        // Set pin PC0 to input
+ GPIOC->MODER &= ~(0x00000003 << (0 * 2)); // Clear mode register
+ GPIOC->MODER |= (0x00000000 << (0 * 2));
+ /* Set mode register (0x00 – Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out) */
+ GPIOC->PUPDR &= ~(0x00000003 << (0 * 2)); // Clear push/pull register
+ GPIOC->PUPDR |= (0x00000002 << (0 * 2));
+ /* Set push/pull register (0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down) */
+
+                        // Set pin PC1 to input
+ GPIOC->MODER &= ~(0x00000003 << (1 * 2)); // Clear mode register
+ GPIOC->MODER |= (0x00000000 << (1 * 2));
+ /* Set mode register (0x00 – Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out) */
+ GPIOC->PUPDR &= ~(0x00000003 << (1 * 2)); // Clear push/pull register
+ GPIOC->PUPDR |= (0x00000002 << (1 * 2));
+ /* Set push/pull register (0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down) */
+}
+
+int8_t readJoystick () {
+/* skriv dette i main for at bruge
+    int j;
+    int i;
+    while (1){
+        readJoystick();
+
+    }
+    */
+
+    uint16_t valUp = GPIOA->IDR & (0x0001 << 4);
+    uint16_t valNed = GPIOB->IDR & (0x0001 << 0);
+    uint16_t valCenter = GPIOB->IDR & (0x0001 << 5);
+    uint16_t valHojre = GPIOC->IDR & (0x0001 << 0);
+    uint16_t valVenstre = GPIOC->IDR & (0x0001 << 1);
+    int8_t valJ;
+    int j=0;
+    int i=0;
+
+        if (valUp > 0 && i==0) {
+            valJ= 0x01;
+            i=1;
+        }
+         else if (valNed > 0 && i==0) {
+            valJ= 0x02;
+            i=1;
+        }
+        else if (valCenter > 0 && i==0) {
+            valJ= 0x10;
+            i=1;
+        }
+        else if (valHojre > 0 && i==0) {
+            valJ= 0x08;
+            i=1;
+        }
+        else if (valVenstre > 0 && i==0) {
+            valJ= 0x04;
+            i=1;
+        } else {
+            valJ = 0x00;
+            i=0;
+        }
+
+
+    if (i!=0) {
+        return valJ;
+        i=0;
+    }
+}
+
+void initLED() {
+ RCC->AHBENR |= RCC_AHBPeriph_GPIOA;
+ RCC->AHBENR |= RCC_AHBPeriph_GPIOB;
+ RCC->AHBENR |= RCC_AHBPeriph_GPIOC;
+  // Enable clock for GPIO Port A through C
+
+
+  // Set pin PA9 to output */
+ GPIOA->OSPEEDR &= ~(0x00000003 << (9 * 2)); // Clear speed register
+ GPIOA->OSPEEDR |= (0x00000002 << (9 * 2));
+ /* set speed register (0x01 - 10 MHz, 0x02 - 2 MHz, 0x03 - 50 MHz) */
+
+ GPIOA->OTYPER &= ~(0x0001 << (9)); // Clear output type register
+ GPIOA->OTYPER |= (0x0000 << (9));
+ /* Set output type register (0x00 - Push pull, 0x01 - Open drain) */
+
+ GPIOA->MODER &= ~(0x00000003 << (9 * 2)); // Clear mode register
+ GPIOA->MODER |= (0x00000001 << (9 * 2));
+ /* Set mode register (0x00 – Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out) */
+
+
+
+ // Set pin PB4 to output */
+ GPIOB->OSPEEDR &= ~(0x00000003 << (4 * 2)); // Clear speed register
+ GPIOB->OSPEEDR |= (0x00000002 << (4 * 2));
+ /* set speed register (0x01 - 10 MHz, 0x02 - 2 MHz, 0x03 - 50 MHz) */
+
+ GPIOB->OTYPER &= ~(0x0001 << (4)); // Clear output type register
+ GPIOB->OTYPER |= (0x0000 << (4));
+ /* Set output type register (0x00 - Push pull, 0x01 - Open drain) */
+
+ GPIOB->MODER &= ~(0x00000003 << (4 * 2)); // Clear mode register
+ GPIOB->MODER |= (0x00000001 << (4 * 2));
+ /* Set mode register (0x00 – Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out) */
+
+
+
+ // Set pin PC7 to output */
+ GPIOC->OSPEEDR &= ~(0x00000003 << (7 * 2)); // Clear speed register
+ GPIOC->OSPEEDR |= (0x00000002 << (7 * 2));
+ /* set speed register (0x01 - 10 MHz, 0x02 - 2 MHz, 0x03 - 50 MHz) */
+
+ GPIOC->OTYPER &= ~(0x0001 << (7)); // Clear output type register
+ GPIOC->OTYPER |= (0x0000 << (7));
+ /* Set output type register (0x00 - Push pull, 0x01 - Open drain) */
+
+ GPIOC->MODER &= ~(0x00000003 << (7 * 2)); // Clear mode register
+ GPIOC->MODER |= (0x00000001 << (7 * 2));
+ /* Set mode register (0x00 – Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out) */
+
+}
+
+void setLED(int rgb[3]) {
+
+    GPIOA->ODR |= (0x0001 << 9); //Set pin PA9 to high = off
+    GPIOB->ODR |= (0x0001 << 4); //Set pin PA9 to high = off
+    GPIOC->ODR |= (0x0001 << 7); //Set pin PA9 to high = off
+
+    //tænder blå
+    if(rgb[2]==1) {
+        GPIOA->ODR &= ~(0x0001 << 9); //Set pin PA9 to low = on
+    }
+    // tænder rød
+    if(rgb[0]==1) {
+        GPIOB->ODR &= ~(0x0001 << 4); //Set pin PA9 to low = on
+    }
+    // tænder grøn
+    if(rgb[1]==1) {
+        GPIOC->ODR &= ~(0x0001 << 7); //Set pin PA9 to low = on
+    }
+}
+
+void LEDjoystick() {
+ /* skriv dette i main for at bruge
+    int j;
+    int i;
+    while (1){
+        LEDjoystick();
+
+    }
+    */
+
+    uint16_t valUp = GPIOA->IDR & (0x0001 << 4);
+    uint16_t valNed = GPIOB->IDR & (0x0001 << 0);
+    uint16_t valCenter = GPIOB->IDR & (0x0001 << 5);
+    uint16_t valHojre = GPIOC->IDR & (0x0001 << 0);
+    uint16_t valVenstre = GPIOC->IDR & (0x0001 << 1);
+    int valJ[3]={0,0,0};
+    int j;
+    int i;
+
+        if (valUp > 0) {
+            valJ[0]= 1;
+            i=1;
+        }
+         else if (valNed > 0) {
+            valJ[1]= 1;
+            i=1;
+        }
+        else if (valCenter > 0) {
+            valJ[2]= 1;
+            i=1;
+        }
+        else if (valHojre > 0) {
+            valJ[1]= 1;
+            valJ[2]= 1;
+            i=1;
+        }
+        else if (valVenstre > 0) {
+            valJ[0]= 1;
+            valJ[1]= 1;
+            i=1;
+        } else {
+
+            i=0;
+        }
+
+    if (j!=i) {
+        setLED(valJ);
+        j = i;
+    }
+}
+
+
+void initTimer (){
+ RCC->APB1ENR |= RCC_APB1Periph_TIM2; // Enable clock line to timer 2;
+ TIM2->CR1 = 0x0000; // Configure timer 2
+ TIM2->ARR = 0x03E7; // Set reload value
+ TIM2->PSC = 0x027F; // Set prescale value
+ TIM2->DIER |= 0x0001; // Enable timer 2 interrupts
+ NVIC_SetPriority(0x001C, 0x0); // Set interrupt priority
+ NVIC_EnableIRQ(0x001C); // Enable interrupt
+// // TIM2->CR1 = 0x0001; --- start timer 2 -- skriv dette i main for at starte timeren
+}
+
+ /* void TIM2_IRQHandler(void) {
+        //Do whatever you want here, but make sure it doesn’t take too much time!
+        // OMREGN TIL SEKUNDER! fra 1/100 dele s
+    time++;
+    printf("%d\n",time);
+
+ TIM2->SR &= ~0x0001; // Clear interrupt bit
+ } */
+//----------------
+/* int8_t timerJoystick () {
+
+    uint16_t valUp = GPIOA->IDR & (0x0001 << 4);
+    uint16_t valNed = GPIOB->IDR & (0x0001 << 0);
+    uint16_t valCenter = GPIOB->IDR & (0x0001 << 5);
+    uint16_t valHojre = GPIOC->IDR & (0x0001 << 0);
+    uint16_t valVenstre = GPIOC->IDR & (0x0001 << 1);
+
+        if (valUp > 0) {
+            return  0x01;
+        }
+        else if (valNed > 0) {
+            return  0x02;
+        }
+        else if (valCenter > 0) {
+            return  0x10;
+        }
+        else if (valHojre > 0) {
+            return  0x08;
+        }
+        else if (valVenstre > 0) {
+            return  0x04;
+        } else {
+            return  0x00;
+        }
+} */
+
+void timer () {
+    while(1) {
+
+        int i=0;
+        int j=1;
+        int k=0;
+        int l_old=0;
+            if (readJoystick()==0x00 && i!=j) {
+                while (1) {
+
+                    if (readJoystick()==0x10) {
+                        TIM2->CR1 = 0x0001;
+                        j=i;
+                        l_old=1;
+                        break;
+                    }
+                    if (readJoystick()==0x01) {
+                        i=0;
+                        j=1;
+
+                        time=0;
+                        s=0;
+                        m=0;
+                        h=0;
+                        gotoxy(2,0);
+                        clreol();
+                        printf("%02d.%02d.%02d\n",h,m,s);
+                        gotoxy(2,0);
+                    }
+                    if (readJoystick()==0x02) {
+                        k=1;
+                        break;
+                    }
+                }
+            }
+
+            if(k==1) {
+                break;
+            }
+
+            else if (i==j) {
+                while (1) {
+
+                    if (s!=s_) {
+                        printf("%02d.%02d.%02d\n",h,m,s);
+                        gotoxy(2,0);
+                        (s_)=(s);
+                        l_old=0;
+                    }
+                    if (readJoystick()==0x08) {
+                           gotoxy(3,0);
+                           printf("Split 1: %02d.%02d.%02d\n",h,m,s);
+                           gotoxy(2,0);
+                           l_old=0;
+                    }
+                    if (readJoystick()==0x04) {
+                           gotoxy(4,0);
+                           printf("Split 2: %02d.%02d.%02d\n",h,m,s);
+                           gotoxy(2,0);
+                           l_old=0;
+                    }
+                    if (readJoystick()==0x10 && l_old==0) {
+                        TIM2->CR1 = 0x0000;
+                        i=0;
+                        j=1;
+                        break;
+                    }
+
+                }
+            }
+    }
+}
+
+void TIM2_IRQHandler(void) {
+
+
+        //Do whatever you want here, but make sure it doesn’t take too much time!
+        // OMREGN TIL SEKUNDER! fra 1/100 dele sek
+        // dette er hvad der sker ved en interrupt i timer 2
+    if (time>=100) {
+            s++;
+            time -= 100;
+            if (s>=60) {
+                m++;
+                s=0;
+            }
+            if (m>=60) {
+                h++;
+                m=0;
+            }
+    }
+    time++;
+    TIM2->SR &= ~0x0001; // Clear interrupt bit
+ }
+
+int8_t get_char(char * text, int length) {
+
+
+
+    if (buffCount<length) {
+        if (0<uart_get_count()) {
+            text[buffCount]=uart_get_char();
+            buffCount++;
+        }
+        if (text[buffCount-1]==0x0d) {
+            buffCount--;
+            text[buffCount]=0x00;
+            return text;
+            uart_clear();
+        }
+    }
+    if(buffCount==length){
+        text[buffCount]=0x00;
+        return text;
+        uart_clear();
+    }
+}
+
+void PCtimer () {
+    printf("STOPWATCH.exe\n");
+    while(1) {
+        int input;
+        int i=0;
+        int j=1;
+        int k=0;
+        int l_old=0;
+            if (i!=j) {
+                while (1) {
+                    input=0;
+                    input=userInput();
+                    if (input==0x10) {
+                        TIM2->CR1 = 0x0001;
+                        j=i;
+                        l_old=1;
+                        break;
+                    }
+                    else if (input==0x01) {
+                        i=0;
+                        j=1;
+
+                        time=0;
+                        s=0;
+                        m=0;
+                        h=0;
+                        //gotoxy(2,0);
+                        clreol();
+                        printf("%02d.%02d.%02d\n",h,m,s);
+                        //gotoxy(2,0);
+                    }
+                    //else if (input==0x02) {
+                       // k=1;
+                        //break;
+                    //}
+                }
+            }
+
+            //if(k==1) {
+               // break;
+            //}
+
+            if (i==j) {
+                input=0;
+                //input=userInput();
+                    while (1) {
+                        if (uart_get_char()!=0) {
+                                input=userInput();
+                        }
+                        if (s!=s_) {
+                            printf("%02d.%02d.%02d\n",h,m,s);
+                            // gotoxy(2,0);
+                            (s_)=(s);
+                            l_old=0;
+                        }
+                        if (input==0x08) {
+                            gotoxy(3,0);
+                            printf("Split 1: %02d.%02d.%02d\n",h,m,s);
+                            gotoxy(2,0);
+                            l_old=0;
+                            input=0;
+                        }
+                        if (input==0x04) {
+                           gotoxy(4,0);
+                           printf("Split 2: %02d.%02d.%02d\n",h,m,s);
+                           gotoxy(2,0);
+                           l_old=0;
+                           input=0;
+                        }
+                        if (input==0x10 && l_old==0) {
+                            TIM2->CR1 = 0x0000;
+                            i=0;
+                            j=1;
+                        break;
+                    }
+
+                }
+            }
+    }
+}
+
+int userInput() {
+
+    char str[21];
+
+    while(1) {
+        get_char(str,20);
+
+        if (strcmp(str,"start")==0) {
+            return 0x10;
+            break;
+        } else if (strcmp(str,"stop")==0 ) {
+            return 0x10;
+            break;
+        } else if (strcmp(str,"split1" )==0) {
+            return 0x08;
+            break;
+        } else if (strcmp(str,"split2")==0 ) {
+            return 0x04;
+            break;
+        } else if (strcmp(str,"reset" )==0) {
+            return 0x01;
+            break;
+        }
+        else {
+            printf("Input not valid.\n");
+        }
+    }
+}
---
+++ src/ansi.c
@@ -0,0 +1,207 @@
+
+#include "ansi.h"
+#include "lut.h"
+#define ESC 0x1B
+#define FIX14_SHIFT 14
+#define FIX14_MULT(a, b) ( (a)*(b) >> FIX14_SHIFT )
+
+
+void fgcolor(int foreground) {
+/*  Value      foreground     Value     foreground
+    ------------------------------------------------
+      0        Black            8       Dark Gray
+      1        Red              9       Light Red
+      2        Green           10       Light Green
+      3        Brown           11       Yellow
+      4        Blue            12       Light Blue
+      5        Purple          13       Light Purple
+      6        Cyan            14       Light Cyan
+      7        Light Gray      15       White
+*/
+  int type = 22;             // normal text
+	if (foreground > 7) {
+	  type = 1;                // bold text
+		foreground -= 8;
+	}
+  printf("%c[%d;%dm", ESC, type, foreground+30);
+}
+
+void bgcolor(int background) {
+/* IMPORTANT:   When you first use this function you cannot get back to true white background in HyperTerminal.
+   Why is that? Because ANSI does not support true white background (ANSI white is gray to most human eyes).
+                The designers of HyperTerminal, however, preferred black text on white background, which is why
+                the colors are initially like that, but when the background color is first changed there is no
+ 	              way comming back.
+   Hint:        Use resetbgcolor(); clrscr(); to force HyperTerminal into gray text on black background.
+
+    Value      Color
+    ------------------
+      0        Black
+      1        Red
+      2        Green
+      3        Brown
+      4        Blue
+      5        Purple
+      6        Cyan
+      7        Gray
+*/
+  printf("%c[%dm", ESC, background+40);
+}
+
+void color(int foreground, int background) {
+// combination of fgcolor() and bgcolor() - uses less bandwidth
+  int type = 22;             // normal text
+	if (foreground > 7) {
+	  type = 1;                // bold text
+		foreground -= 8;
+	}
+  printf("%c[%d;%d;%dm", ESC, type, foreground+30, background+40);
+}
+
+void resetbgcolor() {
+// gray on black text, no underline, no blink, no reverse
+  printf("%c[m", ESC);
+}
+
+void clrscr(void) {
+    printf("\e[2J");
+}
+
+void clreol(void) {
+    printf("\e[K");
+}
+
+void gotoxy(int x , int y) {
+    printf("\e[%d;%dH",x,y);
+}
+
+void underline(uint8_t on) {
+    if (on==1) {
+        printf("\e[4m");
+    }
+    else if (on==0) {
+        printf("\e[24m");
+    }
+}
+
+void window(int x1,int y1, int x2, int y2,int style,char title[]) {
+    gotoxy(x1,y1);
+    int spc = y1-1;
+    int j;
+    int i;
+    if (style==1) {
+        for (i = 0; i <= x2; i++ ) {
+            for (j = 0; j <= y2; j++ ) {
+                    if (i == 0 && j == 0) {
+                        printf("%c",201);              // øverst venstre hjørne
+                    }
+                    else if (i == 0 && j == y2) {
+                        printf("%c\n%*c",187,spc,' '); // øverst højre hjørne
+                    }
+                    else if (i == x2 && j == 0) {
+                        printf("%c",200);               // nederst venstre hjørne
+                    }
+                    else if (i == x2 && j == y2) {
+                        printf("%c\n%*c",188,spc,' ');  // nederst højre hjørne
+                    }
+                    else if (j==0 && (i !=0 && i!=x2 )) {
+                        printf("%c",186);                 // venstre væg
+                    }
+                    else if (j==y2 && (i !=0 || i!=x2 )) {
+                        printf("%c\n%*c",186,spc,' ');   // højre væg
+                    }
+                    else if ((i == 0 || i == x2) && (j !=0 || j != y2 ) ) {
+                        printf("%c", 205);               // gulv/loft
+                    }
+                    else {
+                        printf(" ") ;
+                    }
+            }
+        }
+    }
+    else if (style==2) {
+        for (i = 0; i <= x2; i++ ) {
+            for (j = 0; j <= y2; j++ ) {
+                    if (i == 0 && j == 0) {
+                        printf("%c",218);              // øverst venstre hjørne
+                    }
+                    else if (i == 0 && j == y2) {
+                        printf("%c\n%*c",191,spc,' '); // øverst højre hjørne
+                    }
+                    else if (i == x2 && j == 0) {
+                        printf("%c",192);               // nederst venstre hjørne
+                    }
+                    else if (i == x2 && j == y2) {
+                        printf("%c\n%*c",217,spc,' ');  // nederst højre hjørne
+                    }
+                    else if (j==0 && (i !=0 && i!=x2 )) {
+                        printf("%c",179);                 // venstre væg
+                    }
+                    else if (j==y2 && (i !=0 || i!=x2 )) {
+                        printf("%c\n%*c",179,spc,' ');   // højre væg
+                    }
+                    else if ((i == 0 || i == x2) && (j !=0 || j != y2 ) ) {
+                        printf("%c", 196);               // gulv/loft
+                    }
+                    else {
+                        printf(" ") ;
+                    }
+            }
+        }
+    }
+    gotoxy(x1,y1+2);
+    printf("%c",185);
+    printf(" %s ",title);
+    printf("%c",204);
+    gotoxy(1,1);
+}
+
+//------------------------------------------
+//Herfra implementeres Jespers kode fra opg3
+//------------------------------------------
+void printFix(int32_t i) {
+ // Prints a signed 16.16 fixed point number
+ if ((i & 0x80000000) != 0) { // Handle negative numbers
+ printf("-");
+ i = ~i + 1;
+ }
+ printf("%ld.%04ld", i >> 16, 10000 * (uint32_t)(i & 0xFFFF) >> 16);
+ // Print a maximum of 4 decimal digits to avoid overflow
+ }
+
+int32_t expand(int32_t i) {
+ // Converts an 18.14 fixed point number to 16.16
+ return i << 2;
+ }
+
+ void fix(int32_t i){
+ printFix(expand(i));
+ }
+int32_t calcsin(int deg)
+{
+int32_t input = (deg*512)/360;
+return (int32_t)(sinus[input&0x1FF]);
+}
+int32_t calccos(int deg)
+{
+int32_t input = ((deg+90)*512)/360;
+return (int32_t)(sinus[input&0x1FF]);
+}
+
+struct vector_t {
+ int32_t x, y;
+ };
+
+
+
+//void initVector(struct vector_t *v) {
+ //(*v).x = -4<<14;
+ //(*v).y = 2<<14;
+ //};
+
+int32_t rotatev(struct vector_t *v, int32_t deg) {
+(*v).x=FIX14_MULT((*v).x,calccos(deg))-FIX14_MULT((*v).y,calcsin(deg));
+(*v).y=FIX14_MULT((*v).x,calcsin(deg))+FIX14_MULT((*v).y,calccos(deg));
+return (*v).x, (*v).y;
+
+};
