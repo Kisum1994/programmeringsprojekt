bbd951138a1595941592aef2cf45c7bc8a93c9be
Kisum1994 <46593938+Kisum1994@users.noreply.github.com>
2019-01-11 10:48:08 +0100

    Add files via upload

---
+++ inc/GPIO.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#ifdef _GPIO_H_
+#define _GPIO_H_
+
+// funktioner
+void initJoystick ();
+int8_t readJoystick ();
+#endif
---
+++ inc/ansi.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#ifdef _ANSI_H_
+#define _ANSI_H_
+#define FIX14_SHIFT 14
+#define FIX14_MULT(a, b) ( (a)*(b) >> FIX14_SHIFT )
+
+
+void fgcolor(uint8_t foreground);
+void bgcolor(uint8_t background);
+void color(uint8_t foreground, uint8_t background);
+void gotoxy(int x;int y)
+void clrscr(void);
+void clreol(void);
+void underline(uint8_t on);
+void window(int x1,int y1, int x2, int y2, int style,char title[]);
+
+#endif /* _ANSI_H_ */
